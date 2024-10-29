66365b749d6da121f4c6006331d6cddd05ced88a
sanho50001 <sanho50001@bk.ru>
2024-10-29 15:41:58 +0300

    feature-ppp-68. API для новых пользователей.

--- src/settings/mail.py
+++ src/settings/mail.py
@@ -2,7 +2,7 @@
 import os
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-SMTP_PORT = int(os.getenv("MAIL_SETTINGS_PORT"))
+EMAIL_PORT = int(os.getenv("MAIL_SETTINGS_PORT"))
 EMAIL_HOST = os.getenv("MAIL_SETTINGS_SERVER")
 EMAIL_HOST_USER = os.getenv("MAIL_SETTINGS_EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = os.getenv("MAIL_SETTINGS_EMAIL_PASSWORD")
