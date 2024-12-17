9c8f86c6ee8a8dd1836a5d5c5e04c57d722d3fe2
MaskedGod <134432872+MaskedGod@users.noreply.github.com>
2024-12-18 03:47:26 +0400

    format tools_in_project.py

--- src/projects/models/tools_in_project.py
+++ src/projects/models/tools_in_project.py
@@ -6,7 +6,16 @@ from .participants_in_project import ParticipantInProject
 
 
 class ToolsInProject(models.Model):
-    participant = models.ForeignKey(to="ParticipantInProject", on_delete=models.CASCADE, verbose_name=_("Participant"))
-    tool = models.ForeignKey(to=Tool, on_delete=models.SET_NULL, null=True, verbose_name=_("Tool"))
-    project = models.ForeignKey(to="Project", on_delete=models.CASCADE, verbose_name=_("Project"))
-    participants_needed = models.PositiveIntegerField(null=True, blank=True, verbose_name=_("Participants needed"))
+    participant = models.ForeignKey(
+        to="ParticipantInProject", on_delete=models.CASCADE, verbose_name=_("Participant")
+    )
+    tool = models.ForeignKey(
+        to=Tool, on_delete=models.SET_NULL, null=True, verbose_name=_("Tool")
+    )
+    project = models.ForeignKey(
+        to="Project", on_delete=models.CASCADE, verbose_name=_("Project")
+    )
+    participants_needed = models.PositiveIntegerField(
+        null=True, blank=True, verbose_name=_("Participants needed")
+    )
+    
