bb162d1ae1e8f3c50143d1e0142dd376afea880b
MaskedGod <134432872+MaskedGod@users.noreply.github.com>
2024-12-18 03:46:47 +0400

    format roles_in_project.py

--- src/projects/models/roles_in_project.py
+++ src/projects/models/roles_in_project.py
@@ -6,7 +6,15 @@ from .participants_in_project import ParticipantInProject
 
 
 class RolesInProject(models.Model):
-    participant = models.ForeignKey(to="ParticipantInProject", on_delete=models.CASCADE, verbose_name=_("Participant"))
-    role = models.ForeignKey(to=Role, on_delete=models.SET_NULL, null=True, verbose_name=_("Role"))
-    project = models.ForeignKey(to="Project", on_delete=models.CASCADE, verbose_name=_("Project"))
-    participants_needed = models.PositiveIntegerField(null=True, blank=True, verbose_name=_("Participants needed"))
+    participant = models.ForeignKey(
+        to="ParticipantInProject", on_delete=models.CASCADE, verbose_name=_("Participant")
+    )
+    role = models.ForeignKey(
+        to=Role, on_delete=models.SET_NULL, null=True, verbose_name=_("Role")
+    )
+    project = models.ForeignKey(
+        to="Project", on_delete=models.CASCADE, verbose_name=_("Project")
+    )
+    participants_needed = models.PositiveIntegerField(
+        null=True, blank=True, verbose_name=_("Participants needed")
+    )
